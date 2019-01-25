ea3860fe35d9603a0bde5867c3d4cd78fb399b66
leonardotorres <leonardotorres.ext@tanner.cl>
2019-01-25 00:55:20 -0300

    tutorial mvc listo buscardo con filtro

---
+++ MVC_Login_Test/Data/MVC_Login_TestContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Login_Test.Models
+{
+    public class MVC_Login_TestContext : DbContext
+    {
+        public MVC_Login_TestContext (DbContextOptions<MVC_Login_TestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_Login_Test.Models.Movie> Movie { get; set; }
+    }
+}
